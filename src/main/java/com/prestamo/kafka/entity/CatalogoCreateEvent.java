27622d6893ad2682e67c9e89f0d27b4a949f278a
mals24 <marceloliendo24@gmail.com>
2024-09-18 22:15:34 -0500

    Prueba

---
+++ src/main/java/com/prestamo/kafka/entity/CatalogoCreateEvent.java
@@ -0,0 +1,9 @@
+package com.prestamo.kafka.entity;
+
+import com.prestamo.entity.Catalogo;
+import com.prestamo.kafka.config.Event;
+
+public class CatalogoCreateEvent extends Event<Catalogo>{
+
+	
+}
